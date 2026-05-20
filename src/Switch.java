f6eb25f1bc56fa0d365c45e9cfbee6f4c35ff421
Vincent Macharia Kimani <vincentkimani@Vincents-MacBook-Pro.local>
2026-05-20 10:53:12 +0300

    performed consolidation in switch

--- src/Switch.java
+++ src/Switch.java
@@ -9,13 +9,8 @@ class Switch{
         day = scanner.nextLine();
 
         switch(day){
-            case "Monday" -> System.out.println("It's a week day");
-            case "Tuesday" -> System.out.println("It's a week day");
-            case "Wednesday" -> System.out.println("It's a week day");
-            case "Thursday" -> System.out.println("It's a week day");
-            case "Friday" -> System.out.println("It's a week day");
-            case "Saturday" -> System.out.println("It's a weekend");
-            case "Sunday" -> System.out.println("It's a weekend");
+            case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" -> System.out.println("It's a week day");
+            case "Saturday", "Sunday" -> System.out.println("It's a weekend");
 
             default -> System.out.printf("%s Is not a day!", day );
         }
