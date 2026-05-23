f49c6dafe733fba68d17d735f54e7cb4e7ad04ca
Vincent Macharia Kimani <vincentkimani@Vincents-MacBook-Pro.local>
2026-05-23 15:10:03 +0300

    wip arrays

--- src/OOP/Arrays.java
+++ src/OOP/Arrays.java
@@ -1,4 +1,5 @@
 package OOP;
+import java.util.Scanner;
 
 class Arrays{
     public static void main(String[] args){
@@ -13,7 +14,26 @@ class Arrays{
 //        Enhanced for loop
 //        For every fruit in my array of fruits do this: ...
         for(String fruit: fruits){
-            System.out.printf(fruit);
+            System.out.println(fruit);
         }
+
+        Scanner scanner = new Scanner(System.in);
+        String[] foods;
+        int size;
+
+        System.out.print("\nWhat number of food do you want: ");
+        size = scanner.nextInt();
+        scanner.nextLine();
+        foods = new String[size];
+
+        for(int i =0; i < foods.length; i++){
+            System.out.print("Enter the food you want: ");
+            foods[i] = scanner.nextLine();
+        }
+
+        for(String food: foods){
+            System.out.println(food);
+        }
+
     }
 }
